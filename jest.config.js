e20bab68eaa6a758fc9aa3fad59ce70535e6a67c
Tidbits JS <tidbitsjs@gmail.com>
2022-03-31 23:49:26 +0530

    jest setup

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  // a set of setting that give typescript support to jest
+  // preset: 'ts-jest',
+
+  // transform ts|tsx to a jest understandable format
+  // transform: {
+  // 	'.ts|tsx': 'ts-jest',
+  // },
+  // mocking and transforming of css modules and other files
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/src/__mocks__/fileMock.js",
+    "\\.(css|less)$": "identity-obj-proxy",
+  },
+  // important to expose all the jest-dom api globally in each test
+  setupFilesAfterEnv: ["@testing-library/jest-dom/"],
+};
--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -1,75 +1,15 @@
-import { render, screen } from "@testing-library/react";
+import React from "react";
+import { render } from "@testing-library/react";
 import Header from "../Header";
 
-// @@@ with getByTexts
-it("should render same text passed into title prop", () => {
-  render(<Header title="todo" />);
-  const h1Element = screen.getByText(/todo/i);
-  expect(h1Element).toBeInTheDocument();
+describe("Header", () => {
+  it("should display the prop", () => {
+    // test the component
+    const { getByText } = render(<Header title="test" />);
+    expect(getByText("test")).toBeInTheDocument();
+
+    // test the component with a different prop
+    const { getByText: getByText2 } = render(<Header title="test2" />);
+    expect(getByText2("test2")).toBeInTheDocument();
+  });
 });
-
-/*
-
-    // @@@ with getByRole
-    it("should render same text passed into title prop", () => {
-        render(<Header title="todo" />);
-        const h1Element = screen.getByRole("heading", { name: /todo/i });
-        expect(h1Element).toBeInTheDocument();
-    });
-
-*/
-
-/*
-  
-    // @@@ with getByTitles
-    it("should render same text passed into title prop", () => {
-        render(<Header title="todo" />);
-        const h1Element = screen.getByTitle("Header");
-        expect(h1Element).toBeInTheDocument();
-    });
-
-*/
-
-/*
-
-    // @@@ with getByTestId
-    it("should render same text passed into title prop", () => {
-        render(<Header title="todo" />);
-        const h2Element = screen.getByTestId("header-2");
-        expect(h2Element).toBeInTheDocument();
-    });
-
-*/
-
-/*
-
-    // @@@ with findByText
-    it("should render same text passed into title prop", async () => {
-        render(<Header title="todo" />);
-        const h1Element = await screen.findByText(/todo/i);
-        expect(h1Element).toBeInTheDocument();
-    });
-
-*/
-
-/*
-
-    // @@@ with QUERYBY
-    it("should render same text passed into title prop", () => {
-        render(<Header title="todo" />);
-        const h1Element = screen.queryByText(/dogs/i);
-        expect(h1Element).not.toBeInTheDocument();
-    });
-
-*/
-
-/*
-
-    @@@ with GETALLBYTEXT
-    it("should render same text passed into title prop", () => {
-        render(<Header title="todo" />);
-        const h1Elements = screen.getAllByAltText(/todo/i);
-        expect(h1Elements.length).toBe(1);
-    });
-
-*/
